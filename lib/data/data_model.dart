923537512f1f79dc9e9dc935ae93e59eb289598d
ssarah <sarahmedhat820@gmail.com>
2026-02-07 21:53:19 +0200

    sarah cf

--- lib/data/data_model.dart
+++ lib/data/data_model.dart
@@ -4,37 +4,37 @@ final List<ItemModel> items = [
   ItemModel(
     title: "Samsung 65-Inch Neo QLED 4K Smart TV QA6...",
     rate: "4.5 (132 reviews)",
-    price: 32.899 ,
+    price:  32899.0  ,
     image: 'assets/images/snmsung.png', name: 'Snmsung',
   ),
   ItemModel(
     title: "Apple 2025 MacBook Air 13-inch Laptop with....",
     rate: "4.5 (132 reviews)",
-    price: 51.000 ,
+    price: 51000.0 ,
     image: 'assets/images/laptop.png', name: 'Apple',
   ),
   ItemModel(
     title: "POLAR Ignite 3 GPS Smartwatch iPhone ....",
     rate: "4.5 (132 reviews)",
-    price: 19.500 ,
+    price:  19500.0 ,
     image: "assets/images/image 48.png", name: 'POLAR',
   ),
   ItemModel(
     title: "Soundcore by Anker Life Q30 Hybrid Active Noise...",
     rate: "4.5 (132 reviews)",
-    price: 3.950 ,
+    price: 3950.0 ,
     image: "assets/images/sound.png", name: 'Anker',
   ),
   ItemModel(
     title: "HONOR 400 5G Smartphone, 200MP AI ....",
     rate: "4.5 (132 reviews)",
-    price: 21.999 ,
+    price:21999.0,
     image: "assets/images/phone.png", name: 'HONOR',
   ),
   ItemModel(
     title: "JBL Go 4 Portable Bluetooth Speaker with I...",
     rate: "4.5 (132 reviews)",
-    price: 1.899 ,
+    price: 1899.0,
     image: "assets/images/jpl.png", name: 'JBL',
   ),
 ];
--- lib/widgets/custom_continar.dart
+++ lib/widgets/custom_continar.dart
@@ -17,7 +17,9 @@ class CustomContinar extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = context.watch<ItemProvider>();
     return Container(
-      padding: EdgeInsets.all(12),
+      width: 160,
+      margin: EdgeInsets.only(right: 12),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: Colors.white,
         //color: AppColors.white,
@@ -26,67 +28,87 @@ class CustomContinar extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Container(
-          //   height: 110,
-          //   width: double.infinity,
-          //   decoration: BoxDecoration(
-          //     color: AppColors.lightGray,
-          //     borderRadius: BorderRadius.circular(12),
-          //   ),
-          //   child: ClipRRect(
-          //     borderRadius: BorderRadius.circular(12),
-          // child:
-          Expanded(child: Image.asset(items.image, fit: BoxFit.contain,)),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: AppColors.gray),
+              child: Center(
+                child: Image.asset(items.image, fit: BoxFit.contain),
+              ),
+            ),
+          ),
           //),
-          SizedBox(height: 10,),
-          Text(items.title, maxLines: 2,
+          SizedBox(height: 10),
+          Text(
+            items.title,
+            maxLines: 2,
             overflow: TextOverflow.ellipsis,
-            style: AppTextstyle.black12,),
-          SizedBox(height: 10,),
+            style: AppTextstyle.black12,
+          ),
+          SizedBox(height: 10),
           Row(
             children: [
-              Icon(Icons.star, color: AppColors.yellow,),
+              Icon(Icons.star, color: AppColors.yellow, size: 12),
               //SizedBox(width: 5,),
-              Text(items.rate, style: AppTextstyle.gray10,
-              ),
+              Text(items.rate, style: AppTextstyle.gray12),
             ],
           ),
           Row(
             children: [
-              Text("${items.price.toStringAsFixed(0)} EGP"),
-              !items.isSelected ? GestureDetector(
-                onTap: () => provider.addCart(items),
-                child: Icon(
-                  Icons.add_circle_outline_sharp, color: Colors.grey,),
-              ) : Container(padding: EdgeInsets.symmetric(horizontal: 4),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16)
-                ),
-                child:  Row(
-                  children: [
-                    IconButton(onPressed: ()=> provider.decrement(items), icon: Icon(Icons.remove)),
-                        Text("${items.quantity}", style:  TextStyle(color:  Colors.white,),),
-                    IconButton(constraints: const BoxConstraints(),
-                        padding: EdgeInsets.zero,
-                        onPressed: () => provider.count(items), icon: Icon(Icons.add,color: Colors.white,))
-                  ],
+              Expanded(
+                child: Text(
+                  "${items.price.toStringAsFixed(2)} EGP",
+                  style: TextStyle(fontSize: 12),
+                  overflow: TextOverflow.ellipsis,
                 ),
               ),
+              !items.isSelected
+                  ? GestureDetector(
+                      onTap: () => provider.addCart(items),
+                      child: Icon(
+                        Icons.add_circle_outline_sharp,
+                        color: Colors.grey,
+                      ),
+                    )
+                  : Container(
+                      padding: EdgeInsets.symmetric(horizontal: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => provider.decrement(items),
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text(
+                            "${items.quantity}",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          IconButton(
+                            constraints: const BoxConstraints(),
+                            padding: EdgeInsets.zero,
+                            onPressed: () => provider.count(items),
+                            icon: Icon(Icons.add, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
 
-              SizedBox(width: 5,),
-              IconButton(onPressed: () {
-                context
-                    .read<ItemProvider>()
-                    .lastAddedItem = items;
-                context.read<ItemProvider>().addCart(items);
-                showModalBottomSheet(context: context,
-                  backgroundColor: Colors.transparent,
-                  builder: (context) => CustomBottomsheet(items: items),)
-
-                ;
-              }, icon: Icon(Icons.add, color: AppColors.blue,))
+              SizedBox(width: 5),
+              IconButton(
+                onPressed: () {
+                  context.read<ItemProvider>().lastAddedItem = items;
+                  context.read<ItemProvider>().addCart(items);
+                  showModalBottomSheet(
+                    context: context,
+                    backgroundColor: Colors.transparent,
+                    builder: (context) => CustomBottomsheet(items: items),
+                  );
+                },
+                icon: Icon(Icons.add, color: AppColors.blue),
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
